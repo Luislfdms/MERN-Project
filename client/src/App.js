289c2e45e8e2260f4682a38bf4834af790e24a44
Sebastien Joseph <sebastienjoseph1999@gmail.com>
2023-11-20 20:23:27 -0500

    Install some dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Login from "./MernComponents/LoginPage";
 import Register from "./MernComponents/RegisterPage";
-import Home from "./MernComponents/HomePage"
+// import Home from "./MernComponents/HomePage"
 import {Routes, Route} from "react-router-dom";
 
 
@@ -12,7 +12,7 @@ function App() {
     <Routes>
       <Route path='/login' element={<Login />} />
       <Route path='/register' element={<Register />} />
-      <Route path='/home' element={<Home />} />
+      {/* <Route path='/home' element={<Home />} /> */}
       {/* <Route path='/followers' element{<Followers />} /> */}
     </Routes>
     </>
