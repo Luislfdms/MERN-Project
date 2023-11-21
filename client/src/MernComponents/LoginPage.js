9473c6c97c70310503d2aef11b35c0e16f294d2c
SebastienJ1999 <82691725+SebastienJ1999@users.noreply.github.com>
2023-11-20 19:35:37 -0500

    Finished Front-end Login

---
+++ client/src/MernComponents/LoginPage.js
@@ -0,0 +1,46 @@
+import {useState, React} from 'react'
+import "./Login.css";
+import Register from "./RegisterPage.js";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+function LoginPage() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        //Having trouble trying to get this to connect, saying 404 error, I'm probably doing something wrong
+        // try {
+        //     const res = await axios.post("/login", {username, password});
+        //     console.log("res", res.data)
+        // } catch (err) {
+        //     console.log(err);
+        // }
+    }
+
+  return (
+    <div className='container-page'>
+    <div className='background-images' style={{backgroundImage:'url("/Images/iStock-1310371524 (1).jpg")'}}>
+
+    </div>
+    <body className='login-container'>
+        <img className='clifford-image' src="/Images/imgonline-com-ua-ReplaceColor-x9TGUbjJLxxRliO4.jpg" alt="clifford-logo"></img>            
+        <h1><b style={{color: "red"}}>Login</b> to your Account</h1>
+        <form className='input-container'>
+            <label className='input-label-border'>
+                <p className='login-text'>Username</p>
+                <input onChange={(e) => setUsername(e.target.value)} type="text" className='input-box' placeholder='Username' />
+                <p className='login-text'>Password</p>
+                <input onChange={(e) => setPassword(e.target.value)} type="password" className='input-box' placeholder='Password'/>
+            <button onClick={handleLogin} className='login-button'>Sign in</button>
+            <button className='forgot-password-button'>Forgot password?</button>
+            <h4>Don't have an account? <Link to='/register' className='sign-up-link'>Sign up</Link></h4> 
+            </label>
+        </form>
+    </body>
+        </div>
+  )
+}
+
+export default LoginPage
