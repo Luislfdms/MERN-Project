8944870bc42341c2e5d5c12e124e7405d73ff119
SebastienJ1999 <82691725+SebastienJ1999@users.noreply.github.com>
2023-11-20 19:41:21 -0500

    Update RegisterName.js

--- client/src/MernComponents/RegisterName.js
+++ client/src/MernComponents/RegisterName.js
@@ -1,29 +1,32 @@
-import React from 'react'
-import Login from "./Login";
+import {React, useState} from 'react'
+import Login from "./LoginPage";
 import "./Register.css";
+import { Link } from "react-router-dom";
 
 function RegisterName() {
+//Use redux in order change the display from the name ask to account info ask.
+//temporary state to see how it would function, would make name display none and account display to 
+const [visibleRegister, setVisibleRegister] = useState(true);  
+const [firstName, setFirstname] = useState("");
+const [lastName, setLastname] = useState("");
+
+const nextRegisterScreen = () => { 
+    setVisibleRegister(!visibleRegister);
+  }
+
   return (
-    <div className='container-page'>
-    <div className='background-images' style={{backgroundImage:'url("/Images/iStock-1310371524 (1).jpg")'}}>
-
-    </div>
-    <body className='register-container'>
-        <img className='clifford-image' src="/Images/imgonline-com-ua-ReplaceColor-x9TGUbjJLxxRliO4.jpg" alt="clifford-logo"></img>            
-        <h1><b style={{color: "red"}}>Register</b> your Account</h1>
-        <form className='input-container'>
-            <label className='input-label-border'>
-                <p className='register-text'>First Name</p>
-                <input type="text" className='input-box' placeholder='First Name'/>
-                <p className='register-text'>Last Name</p>
-                <input type="text" className='input-box' placeholder='Last Name'/>
-
-            <button className='register-button'>Next</button>
-            <h4>Already have an account? <a href="/Login" className='sign-up-link'>Sign in</a></h4>
-            </label>
-            </form>
-    </body>
-        </div>
+    <>
+    {visibleRegister && (
+    <label className='input-label-border'>
+      <p className='register-text'>First Name</p>
+      <input type="text" className='input-box' placeholder='First Name'/>
+      <p className='register-text'>Last Name</p>
+      <input type="text" className='input-box' placeholder='Last Name'/>
+      <button onClick={nextRegisterScreen} className='register-button'>Next</button>
+      <h4>Already have an account? <Link to="/login" className='sign-up-link'>Sign in</Link></h4>
+    </label>
+     )}
+    </>
   )
 }
 
