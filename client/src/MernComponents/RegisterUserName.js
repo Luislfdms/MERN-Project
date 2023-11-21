210f75e8a1d9dd5e3e605572af8272a72bfd968a
SebastienJ1999 <82691725+SebastienJ1999@users.noreply.github.com>
2023-11-20 19:41:50 -0500

    Update RegisterUserName.js

--- client/src/MernComponents/RegisterUserName.js
+++ client/src/MernComponents/RegisterUserName.js
@@ -1,30 +1,23 @@
 import React from 'react'
-import Login from "./Login";
+import Login from "./LoginPage";
 import "./Register.css";
+import { Link } from "react-router-dom";
 
 function RegisterUserName() {
-  return (
-    <div className='container-page'>
-    <div className='background-images' style={{backgroundImage:'url("/Images/iStock-1310371524 (1).jpg")'}}>
+//Use redux in order change the display from the name ask to account info ask.
 
-    </div>
-    <body className='login-container'>
-        <img className='clifford-image' src="/Images/imgonline-com-ua-ReplaceColor-x9TGUbjJLxxRliO4.jpg" alt="clifford-logo"></img>            
-        <h1><b style={{color: "red"}}>Register</b> your Account</h1>
-        <form className='input-container'>
-            <label className='input-label-border'>
-                <p className='register-text'>Email</p>
-                <input type="text" className='input-box' placeholder='Email'/>
-                <p className='register-text'>Username</p>
-                <input type="text" className='input-box' placeholder='User Name'/>
-                <p className='register-text'>Password</p>
-                <input type="text" className='input-box' placeholder='Password'/>
-            <button className='register-button'>Sign up</button>
-            <h4>Already have an account? <a href="/Login" className='sign-up-link'>Sign in</a></h4>
-            </label>
-            </form>
-    </body>
-        </div>
+  return (
+    <label className='input-label-border userNameDisplay'>
+      <p className='register-text'>Email</p>
+      <input type="text" className='input-box' placeholder='Email'/>
+      <p className='register-text'>Username</p>
+      <input type="text" className='input-box' placeholder='User Name'/>
+      <p className='register-text'>Password</p>
+      <input type="text" className='input-box' placeholder='Password'/>
+      <button className='register-button'>Sign up</button>
+      <button className='register-button reverse-back-button'>Back</button>
+      <h4>Already have an account? <Link to="/" className='sign-up-link'>Sign in</Link></h4>
+    </label>
   )
 }
 
