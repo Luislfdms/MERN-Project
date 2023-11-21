0c401851e74011ae7c72b761a870fda6a5c194ee
SebastienJ1999 <82691725+SebastienJ1999@users.noreply.github.com>
2023-11-20 19:46:12 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
-import Login from "./MernComponents/Login";
-import Register from "./MernComponents/Register";
+import Login from "./MernComponents/LoginPage";
+import Register from "./MernComponents/RegisterPage";
+import Home from "./MernComponents/HomePage"
+import {Routes, Route} from "react-router-dom";
 
 
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-      {/* <Register /> */}
-    </div>
+    <>
+    <Routes>
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/home' element={<Home />} />
+      {/* <Route path='/followers' element{<Followers />} /> */}
+    </Routes>
+    </>
   );
 }
 
