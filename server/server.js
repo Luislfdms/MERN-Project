f08b5d3d4d02c660aa711242e1c9edd263da911c
Luislfdms <luislfdms18@gmail.com>
2023-11-22 14:38:40 -0500

    change default port

--- server/server.js
+++ server/server.js
@@ -29,6 +29,6 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 80;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
