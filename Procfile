2a8fea12700e1dab5d35e8f971852567763f9865
Luislfdms <luislfdms18@gmail.com>
2023-11-22 20:23:06 -0500

    please work

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm run start:all
+web: node server/server.js
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const config = require('./config');
 const postAPIRoutes = require('./routes/postAPI');
 const userAPIRoutes = require('./routes/userAPI');
+const path = require("path");
 
 // Connection to database
 require('./dbConnection'); 
@@ -25,16 +26,16 @@ mongoose.connect(config.mongoURI)
 app.use('/postAPI', postAPIRoutes);
 app.use('/userAPI', userAPIRoutes)
 
-
+_dirname = path.resolve();
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(_dirname, "../client/build")));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
