4b20f9676933a050ed28c2548af1bade57cf90bd
Luislfdms <luislfdms18@gmail.com>
2023-11-22 19:31:33 -0500

    serve front-end from express

--- server/server.js
+++ server/server.js
@@ -25,9 +25,15 @@ mongoose.connect(config.mongoURI)
 app.use('/postAPI', postAPIRoutes);
 app.use('/userAPI', userAPIRoutes)
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
