f550c6252f0d018350dcd830523a704791519870
nickllages <52086760+nickllages@users.noreply.github.com>
2023-11-25 09:32:15 -0500

    Update postController.js

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -13,8 +13,6 @@ const createPost = async (req, res) => {
 
   //add post to db
   const user = await User.findOne({username: userID})
-  console.log("user:");
-  console.log(user);
   if(user) {
     try {
       const post = await Post.create({postTitle, postMain, userID, image, upvotes, downvotes})
